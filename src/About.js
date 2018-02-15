5478c1a73b0735b414929190c4f18e78e625ec49
B <nielsen.blake.d@gmail.com>
2018-02-15 01:19:50 -0500

    About.js - Several changes.

--- src/About.js
+++ src/About.js
@@ -34,27 +34,28 @@ class About extends Component {
         Sub-domain: Unlocking N:N value efficiency (scale, scope etc.) in n dimensions.
         This encompasses: dx Identity. Delegating dx trust. Protocol liquidity provision. 
         Future: Acting quick where nascent whitespace of inefficiency lie. (subject to change dynamically).
-        The current state of most domains = overvalued efficiency > dynamism, anti-fragility.</p>
+        i.e. domains that value efficiency at the expense of dynamism & anti-fragility.</p>
 
         <h2 className="About-header">Why?</h2>
         <p className="cypherpunk">Timing. We’re in period of punctuated equilibrium.
         Current potential impact on Global state trajectory per unit time is 
         [arguably] at a local maximum. Peak actionability on trajectory-influence.</p>
 
-        <h2 className="About-header">What is N:N?</h2>
-        <p className="cypherpunk">Creating value relative to any underlying. e.g: </p>
-        <p className="cypherpunk">ERC20 Token [ETH] vs. Non-Fungible Asset. [Home Equity]</p>
-        <p className="cypherpunk">Debt [Collaterized Debt Position] vs. Bag [TRON]</p>
+        <h2 className="About-header">What&apos;s in the name N:N?</h2>  <!–– added apostrophe escpae chars. ––> 
+        <p className="cypherpunk">Direct value exchange between any (n) two assets. N:N = actionable value relations between any ever-expanding n of protocol-enabled assets. <br /> 
+         This marks a unique transition.  example:: </p>
+        <p className="cypherpunk">D.A.O. ERC20 profit shares : previously illiquid, Non-fungible Asset. [Tokenized Home Equity]. <br>
+        <p className="cypherpunk">Debt [Crypto-Collaterized Debt Position] : Bag [TRON]<br> 
 
-        <h2 className="About-header">What is a Collective?</h2>
-        <p className="cypherpunk">Free flowing group of individuals with access to resources with a shared mission to create.</p>
+        <h2 className="About-header">Collective?</h2>
+        <p className="cypherpunk">Free flowing group of individuals with access to resources & a shared mission to create.</p>
 
         <p className="cypherpunk">Our conjecture: trajectory will be primarily be determined by:
         a) Data access/flow; Networks [hardware & wetware]. 
         b) Intelligence; AI</p>
         
         <h2 className="About-header">What?</h2>
-        <p className="cypherpunk">Building is underway. Non-fungible asset networks. Distributed debt/equity/value protocols. </p>
+        <p className="cypherpunk">Building is underway. N:N protocol tooling. Non-fungible asset networks. AI guided, automated resource allocation over dx protocols (i.e. relaying, dynamic order book creation).</p>
 
         <h2 className="About-header">How?</h2>
         <p className="cypherpunk">
